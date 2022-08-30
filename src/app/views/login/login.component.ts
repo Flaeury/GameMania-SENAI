f1946e6f0abc01fa506d84917c2762c7ade3cf85
Raiane <raianw200410@outlook.com>
2022-08-30 00:48:17 -0300

    api user

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +10,29 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   userModel = new User();
 
+  mensagem = ""
+
   receberDados() { 
     console.log(this.userModel)
+
     this.loginService.login(this.userModel).subscribe( (response) => {
-      console.log(response)
+
+      localStorage.setItem("nomeUsuario", response.body.user.nome)
+
+      this.router.navigateByUrl("/")
+
+    }, (respostaErro) => {
+
+      this.mensagem = respostaErro.error
+
+
     } )
   }
 }
